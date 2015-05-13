37fbcb72405a3df11138cd81b1becd35b2973799
kobake <kobake@users.sourceforge.net>
2015-05-13 13:03:05 +0900

    最新版

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace MultipleKeySender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.lstWindows = new System.Windows.Forms.ListView();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.label4 = new System.Windows.Forms.Label();
+			this.edtInput = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.btnRemove = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.edtDetails = new System.Windows.Forms.TextBox();
+			this.btnFindWindow = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.lstWindows);
+			this.splitContainer1.Panel1.Controls.Add(this.label4);
+			this.splitContainer1.Panel1.Padding = new System.Windows.Forms.Padding(4, 4, 2, 4);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.edtInput);
+			this.splitContainer1.Panel2.Controls.Add(this.label6);
+			this.splitContainer1.Panel2.Controls.Add(this.btnRemove);
+			this.splitContainer1.Panel2.Controls.Add(this.label2);
+			this.splitContainer1.Panel2.Controls.Add(this.edtDetails);
+			this.splitContainer1.Panel2.Controls.Add(this.btnFindWindow);
+			this.splitContainer1.Panel2.Controls.Add(this.label1);
+			this.splitContainer1.Panel2.Padding = new System.Windows.Forms.Padding(2, 4, 4, 4);
+			this.splitContainer1.Size = new System.Drawing.Size(676, 380);
+			this.splitContainer1.SplitterDistance = 422;
+			this.splitContainer1.TabIndex = 18;
+			// 
+			// lstWindows
+			// 
+			this.lstWindows.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+			this.lstWindows.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.lstWindows.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.lstWindows.FullRowSelect = true;
+			this.lstWindows.Location = new System.Drawing.Point(4, 24);
+			this.lstWindows.Name = "lstWindows";
+			this.lstWindows.Size = new System.Drawing.Size(416, 352);
+			this.lstWindows.TabIndex = 18;
+			this.lstWindows.UseCompatibleStateImageBehavior = false;
+			this.lstWindows.View = System.Windows.Forms.View.Details;
+			this.lstWindows.SelectedIndexChanged += new System.EventHandler(this.lstWindows_SelectedIndexChanged);
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "HWND";
+			this.columnHeader1.Width = 89;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Class";
+			this.columnHeader2.Width = 88;
+			// 
+			// columnHeader3
+			// 
+			this.columnHeader3.Text = "Text";
+			this.columnHeader3.Width = 78;
+			// 
+			// columnHeader4
+			// 
+			this.columnHeader4.Text = "EXE";
+			this.columnHeader4.Width = 137;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Dock = System.Windows.Forms.DockStyle.Top;
+			this.label4.Location = new System.Drawing.Point(4, 4);
+			this.label4.Name = "label4";
+			this.label4.Padding = new System.Windows.Forms.Padding(4);
+			this.label4.Size = new System.Drawing.Size(93, 20);
+			this.label4.TabIndex = 17;
+			this.label4.Text = "Target windows";
+			// 
+			// edtInput
+			// 
+			this.edtInput.AcceptsReturn = true;
+			this.edtInput.AcceptsTab = true;
+			this.edtInput.Dock = System.Windows.Forms.DockStyle.Top;
+			this.edtInput.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.edtInput.Location = new System.Drawing.Point(2, 236);
+			this.edtInput.Multiline = true;
+			this.edtInput.Name = "edtInput";
+			this.edtInput.Size = new System.Drawing.Size(244, 48);
+			this.edtInput.TabIndex = 40;
+			this.edtInput.Text = "Focus this, and input keys!";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Dock = System.Windows.Forms.DockStyle.Top;
+			this.label6.Location = new System.Drawing.Point(2, 210);
+			this.label6.Name = "label6";
+			this.label6.Padding = new System.Windows.Forms.Padding(4, 10, 4, 4);
+			this.label6.Size = new System.Drawing.Size(197, 26);
+			this.label6.TabIndex = 39;
+			this.label6.Text = "Send key strokes to target windows";
+			// 
+			// btnRemove
+			// 
+			this.btnRemove.Dock = System.Windows.Forms.DockStyle.Top;
+			this.btnRemove.Location = new System.Drawing.Point(2, 178);
+			this.btnRemove.Name = "btnRemove";
+			this.btnRemove.Size = new System.Drawing.Size(244, 32);
+			this.btnRemove.TabIndex = 38;
+			this.btnRemove.Text = "Remove selected window.";
+			this.btnRemove.UseVisualStyleBackColor = true;
+			this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.label2.Location = new System.Drawing.Point(2, 152);
+			this.label2.Name = "label2";
+			this.label2.Padding = new System.Windows.Forms.Padding(4, 10, 4, 4);
+			this.label2.Size = new System.Drawing.Size(129, 26);
+			this.label2.TabIndex = 37;
+			this.label2.Text = "Remove target window";
+			// 
+			// edtDetails
+			// 
+			this.edtDetails.AcceptsReturn = true;
+			this.edtDetails.AcceptsTab = true;
+			this.edtDetails.Dock = System.Windows.Forms.DockStyle.Top;
+			this.edtDetails.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.edtDetails.Location = new System.Drawing.Point(2, 56);
+			this.edtDetails.Multiline = true;
+			this.edtDetails.Name = "edtDetails";
+			this.edtDetails.ReadOnly = true;
+			this.edtDetails.Size = new System.Drawing.Size(244, 96);
+			this.edtDetails.TabIndex = 36;
+			// 
+			// btnFindWindow
+			// 
+			this.btnFindWindow.Dock = System.Windows.Forms.DockStyle.Top;
+			this.btnFindWindow.Location = new System.Drawing.Point(2, 24);
+			this.btnFindWindow.Name = "btnFindWindow";
+			this.btnFindWindow.Size = new System.Drawing.Size(244, 32);
+			this.btnFindWindow.TabIndex = 26;
+			this.btnFindWindow.Text = "Drag this and drop on the target window!";
+			this.btnFindWindow.UseVisualStyleBackColor = true;
+			this.btnFindWindow.Click += new System.EventHandler(this.btnFindWindow_Click);
+			this.btnFindWindow.GiveFeedback += new System.Windows.Forms.GiveFeedbackEventHandler(this.btnFindWindow_GiveFeedback);
+			this.btnFindWindow.MouseCaptureChanged += new System.EventHandler(this.btnFindWindow_MouseCaptureChanged);
+			this.btnFindWindow.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnFindWindow_MouseDown);
+			this.btnFindWindow.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnFindWindow_MouseMove);
+			this.btnFindWindow.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnFindWindow_MouseUp);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.label1.Location = new System.Drawing.Point(2, 4);
+			this.label1.Name = "label1";
+			this.label1.Padding = new System.Windows.Forms.Padding(4);
+			this.label1.Size = new System.Drawing.Size(108, 20);
+			this.label1.TabIndex = 25;
+			this.label1.Text = "Add target window";
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(676, 380);
+			this.Controls.Add(this.splitContainer1);
+			this.Name = "Form1";
+			this.Text = "MultipleKeySender";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel1.PerformLayout();
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			this.splitContainer1.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.ListView lstWindows;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+		private System.Windows.Forms.ColumnHeader columnHeader3;
+		private System.Windows.Forms.ColumnHeader columnHeader4;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button btnFindWindow;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox edtInput;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Button btnRemove;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox edtDetails;
+		private System.Windows.Forms.Timer timer1;
+
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace MultipleKeySender
+{
+
+	public partial class Form1 : Form
+	{
+		public class MessageFilter : IMessageFilter
+		{
+			Form1 form;
+			public MessageFilter(Form1 form)
+			{
+				this.form = form;
+			}
+			public bool PreFilterMessage(ref Message m)
+			{
+				return form.PreFilterMessage(ref m);
+			}
+		}
+
+		MessageFilter messageFilter;
+		Win32dll.KeyboardHookDelegate hookDelegate;
+		IntPtr hook;
+		const int KeyboardHookType = 13;
+		static readonly object EventKeyboardHooked = new object();
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			// リソース
+			cursorTarget = new Cursor(Properties.Resources.target1.Handle);
+			this.Icon = Properties.Resources.app;
+
+			// メッセージ受付
+			messageFilter = new MessageFilter(this);
+			Application.AddMessageFilter(messageFilter);
+
+			// グローバルフック
+			if (Environment.OSVersion.Platform != PlatformID.Win32NT)
+				throw new PlatformNotSupportedException("Windows 98/Meではサポートされていません。");
+			this.hookDelegate = new Win32dll.KeyboardHookDelegate(CallNextHook);
+			IntPtr module = Marshal.GetHINSTANCE(typeof(Form1).Assembly.GetModules()[0]);
+			hook = Win32dll.SetWindowsHookEx(KeyboardHookType, hookDelegate, module, 0);
+			if (hook == IntPtr.Zero)
+			{
+				int errCode = Marshal.GetLastWin32Error();
+
+				StringBuilder message = new StringBuilder(255);
+
+				Win32dll.FormatMessage(
+					Win32dll.FORMAT_MESSAGE_FROM_SYSTEM,
+					IntPtr.Zero,
+					(uint)errCode,
+					0,
+					message,
+					message.Capacity,
+					IntPtr.Zero
+				);
+
+
+				string msg = message.ToString();
+
+
+			}
+		}
+		private int CallNextHook(int code, KeyboardMessage message, ref KeyboardState state)
+		{
+			if (code >= 0)
+				OnKeyboardHooked(new Win32dll.KeyboardHookedEventArgs(message, ref state));
+			return Win32dll.CallNextHookEx(hook, code, message, ref state);
+		}
+		///<summary>
+		///KeyboardHookedイベントを発生させる。
+		///</summary>
+		///<param name="e">イベントのデータ。</param>
+		protected virtual void OnKeyboardHooked(Win32dll.KeyboardHookedEventArgs e)
+		{
+			Win32dll.KeyboardHookedEventHandler handler = base.Events[EventKeyboardHooked] as Win32dll.KeyboardHookedEventHandler;
+			if (handler != null)
+				handler(this, e);
+			// 処理
+			// hogehogehoge
+			edtInput.Text = "hoge";
+		}
+
+		private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			// グローバルフック終了
+			if(hook != IntPtr.Zero){
+				Win32dll.UnhookWindowsHookEx(hook);
+				hook = IntPtr.Zero;
+			}
+
+			// メッセージ受付終了
+			Application.RemoveMessageFilter(messageFilter);
+			messageFilter = null;
+		}
+
+		// リソース
+		Cursor cursorTarget;
+
+		// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
+		// キー送信
+		// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
+		public bool PreFilterMessage(ref Message m)
+		{
+			if (m.HWnd == edtInput.Handle)
+			{
+				if (m.Msg == Win32dll.WM_KEYDOWN || m.Msg == Win32dll.WM_KEYUP)
+				{
+					foreach (WindowInfo win in windows)
+					{
+						if (win != null)
+						{
+							Win32dll.PostMessage(win.hwnd, m.Msg, m.WParam, m.LParam);
+						}
+					}
+					return true;
+				}
+			}
+			return false;
+		}
+
+		// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
+		// ウィンドウ情報設定
+		// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
+//		private WindowInfo[] windows = new WindowInfo[3];
+		List<WindowInfo> windows = new List<WindowInfo>();
+		WindowInfo winTmp = null;
+
+		private void btnFindWindow_MouseDown(object sender, MouseEventArgs e)
+		{
+			winTmp = null;
+			Control control = (Control)sender;
+			control.Capture = true;
+			Cursor.Current = cursorTarget;
+		}
+
+		private void btnFindWindow_MouseMove(object sender, MouseEventArgs e)
+		{
+			Control control = (Control)sender;
+			if (control.Capture)
+			{
+				// 情報取得
+				Point p2 = System.Windows.Forms.Control.MousePosition;
+				IntPtr hwnd = Win32dll.WindowFromPoint(p2.X, p2.Y);
+				if (winTmp == null || winTmp.hwnd != hwnd)
+				{
+					// 旧枠削除
+					if (winTmp != null)
+					{
+						winTmp.EraseInvertFrame();
+					}
+					// 差替え
+					winTmp = new WindowInfo(hwnd);
+					// 情報表示
+					edtDetails.Text = winTmp.ToString();
+					// 新枠描画
+					winTmp.DrawInvertFrame();
+				}
+			}
+		}
+
+		private void btnFindWindow_MouseUp(object sender, MouseEventArgs e)
+		{
+			Control control = (Control)sender;
+			// ウィンドウ確定
+			if (control.Capture && winTmp != null)
+			{
+				// 重複チェック
+				bool bOk = true;
+				foreach (WindowInfo win in windows)
+				{
+					if (win.hwnd == winTmp.hwnd)
+					{
+						bOk = false;
+					}
+				}
+				// 追加処理
+				if (bOk)
+				{
+					windows.Add(winTmp);
+					ListViewItem item = lstWindows.Items.Add("0x" + winTmp.hwnd.ToString("x8"));
+					item.SubItems.Add(winTmp.className);
+					item.SubItems.Add(winTmp.text);
+					item.SubItems.Add(winTmp.exe);
+					item.Tag = winTmp;
+				}
+			}
+			// キャプチャ終了
+			control.Capture = false;
+		}
+
+		private void btnFindWindow_MouseCaptureChanged(object sender, EventArgs e)
+		{
+			// 枠削除
+			if (winTmp != null)
+			{
+				winTmp.EraseInvertFrame();
+			}
+			// キャンセル
+			winTmp = null;
+			edtDetails.Text = "";
+			Cursor.Current = Cursors.Default;
+		}
+
+		// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
+		// ウィンドウ情報解除
+		// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
+		private void btnRemove_Click(object sender, EventArgs e)
+		{
+			foreach (ListViewItem item in lstWindows.SelectedItems)
+			{
+				windows.Remove((WindowInfo)item.Tag);
+				lstWindows.Items.Remove(item);
+			}
+		}
+
+		private void lstWindows_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string details = "";
+			foreach (ListViewItem item in lstWindows.SelectedItems)
+			{
+				WindowInfo win = (WindowInfo)item.Tag;
+				if (win != null)
+				{
+					details += win.ToString() + "\r\n";
+				}
+			}
+			edtDetails.Text = details;
+		}
+
+		// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
+		// 定期処理
+		// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			foreach (ListViewItem item in lstWindows.Items)
+			{
+				WindowInfo win  = (WindowInfo)item.Tag;
+				if (!Win32dll.IsWindowEnabled(win.hwnd))
+				{
+					windows.Remove(win);
+					lstWindows.Items.Remove(item);
+				}
+			}
+		}
+
+		private void btnFindWindow_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnFindWindow_GiveFeedback(object sender, GiveFeedbackEventArgs e)
+		{
+			Control control = (Control)sender;
+			if (control.Capture)
+			{
+			}
+		}
+
+	}
+
+	public class WindowInfo
+	{
+		public IntPtr hwnd;
+		public string className;
+		public string text;
+		public string exe;
+		private bool drawed = false;
+		Rectangle rc = new Rectangle();
+
+		public WindowInfo(IntPtr hwnd)
+		{
+			this.hwnd = hwnd;
+			className = Win32dll.GetClassName(hwnd);
+			text = Win32dll.GetWindowText(hwnd);
+			rc = Win32dll.GetWindowRect(hwnd);
+
+			// プロセスID取得
+			uint pid = 0;
+			Win32dll.GetWindowThreadProcessId(hwnd, out pid);
+//			IntPtr hProcess = Win32dll.OpenProcess(Win32dll.ProcessAccessFlags.QueryInformation, true, pid);
+//			IntPtr hModule = Win32dll.GetWindowLongPtr(hwnd, (int)GWL.GWL_HINSTANCE);
+//			exe = Win32dll.GetModuleFileNameEx(hProcess, hModule);
+//			Win32dll.CloseHandle(hProcess);
+
+			// 実行中のすべてのプロセスを取得する
+			System.Diagnostics.Process[] hProcesses = System.Diagnostics.Process.GetProcesses();
+
+			// 取得できたプロセスからプロセス名を取得する
+			foreach (System.Diagnostics.Process hProcess in hProcesses)
+			{
+				if (hProcess.Id == pid)
+				{
+					exe = hProcess.ProcessName;
+					break;
+				}
+			}
+		}
+
+		public void DrawInvertFrame()
+		{
+			if (drawed) return;
+			ControlPaint.DrawReversibleFrame(rc, Color.Black, FrameStyle.Thick);
+			drawed = true;
+		}
+
+		public void EraseInvertFrame()
+		{
+			if (!drawed) return;
+			ControlPaint.DrawReversibleFrame(rc, Color.Black, FrameStyle.Thick);
+			drawed = false;
+		}
+
+		public string ToString()
+		{
+			string ret = "";
+			ret += "[HWND] 0x" + hwnd.ToString("x8") + "\r\n";
+//			ret += "[rc] " + rc + "\r\n";
+			ret += "[className] " + className + "\r\n";
+			ret += "[text] " + text + "\r\n";
+			return ret;
+		}
+	}
+
+
+
+
+}
+
+
